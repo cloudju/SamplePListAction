d0cf4c1a44b1c0580d219f6c5a8182ae68ea5c1f
cloudju <cloudju1978@gmail.com>
2019-01-28 23:23:39 +0900

    Initial Commit

---
+++ SamplePListAction/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SamplePListAction
+//
+//  Created by 居朝強 on 2019/01/28.
+//  Copyright © 2019 居朝強. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ SamplePListAction/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SamplePListAction
+//
+//  Created by 居朝強 on 2019/01/28.
+//  Copyright © 2019 居朝強. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
